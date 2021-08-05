ab096f59576f1c1be25cca5cd9e0e8ca57dc9c15
Jerome Martin Flaherty <jerbear2@gmail.com>
2021-08-05 17:05:01 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,22 @@
+const mainUrl = 'https://api.publicapis.org/entries';
+
+fetch(mainUrl)
+  .then(res => res.json())
+  .then(json => display(json))
+  .catch(err => console.dir(err))
+
+function display(json) {
+  const verified = json.entries.filter(value => value.Auth == "" && value.Cors == 'yes');
+
+  const list = document.createElement('ul')
+  verified.map((api, index) => {
+    const item = document.createElement('li')
+    const link = document.createElement('a')
+    link.innerText = api.API
+    link.href = api.Link
+
+    item.appendChild(link)
+    list.appendChild(item)
+  })
+  document.body.appendChild(list)
+}
