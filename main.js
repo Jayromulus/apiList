71a8218637d031659c7dcdea6205fcebe18cd158
Jerome Martin Flaherty <jerbear2@gmail.com>
2021-08-09 11:48:23 -0400

    styling added

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-const mainUrl = 'https://api.publicapis.org/entries';
+const mainUrl = 'https://api.publicapis.org/entries'
+const target = document.getElementById('target')
 
 fetch(mainUrl)
   .then(res => res.json())
@@ -8,16 +9,65 @@ fetch(mainUrl)
 function display(json) {
   const verified = json.entries.filter(value => value.Auth == "" && value.Cors == 'yes' && value.HTTPS == true);
   console.log(verified)
-
-  const list = document.createElement('ul')
-  verified.map((api, index) => {
-    const item = document.createElement('li')
-    const link = document.createElement('a')
-    link.innerText = api.API
-    link.href = api.Link
-
-    item.appendChild(link)
-    list.appendChild(item)
+  verified.sort((a, b) => {
+    if(a.Category[0] < b.Category[0]) {
+      return -1
+    } else if(a.Category[0] > b.Category[0]) {
+      return 1
+    } else {
+      return 0
+    }
   })
-  document.body.appendChild(list)
+
+  // const list = document.createElement('ul')
+
+  /*
+  <div class="flip-card">
+    <div class="flip-card-inner">
+      <div class="flip-card-front">
+        <img src="img_avatar.png" alt="Avatar" style="width:300px;height:300px;">
+      </div>
+      <div class="flip-card-back">
+        <h1>John Doe</h1>
+        <p>Architect & Engineer</p>
+        <p>We love that guy</p>
+      </div>
+    </div>
+  </div>
+  */
+  verified.map((api, index) => {
+    const card = document.createElement('div')
+    const inner = document.createElement('div')
+    const front = document.createElement('div')
+    const back = document.createElement('div')
+    const link = document.createElement('a')
+    const frontWords = document.createElement('p')
+    const backWords = document.createElement('p')
+
+    frontWords.innerText = api.API
+    frontWords.style.fontSize = '1.5em'
+    frontWords.style.padding = '0px 5%'
+    backWords.innerText = api.Description
+    backWords.style.fontSize = '1.75em'
+    backWords.style.padding = '0px 5%'
+
+    link.href = api.Link
+    link.target = '_blank'
+    
+
+    card.classList.add('flip-card')
+    inner.classList.add('flip-card-inner')
+    front.classList.add('flip-card-front')
+    back.classList.add('flip-card-back')
+
+    link.appendChild(card)
+    front.appendChild(frontWords)
+    back.appendChild(backWords)
+    inner.appendChild(front)
+    inner.appendChild(back)
+    card.appendChild(inner)
+    target.appendChild(link)
+  })
+  // document.body.appendChild(list)
 }
+
